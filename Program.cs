67133ded306ca4c3b510ad82323dcab8e939a601
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2026-03-17 14:50:43 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 var keyDerivationService = new KeyDerivationService();
 var encryptionService = new EncryptionService();
 var vaultService = new VaultService(keyDerivationService, encryptionService);
-var consoleUiService = new ConsoleUiService(vaultService);
+var passwordGeneratorService = new PasswordGeneratorService();
+var consoleUiService = new ConsoleUiService(vaultService, passwordGeneratorService);
 
 consoleUiService.Run();
