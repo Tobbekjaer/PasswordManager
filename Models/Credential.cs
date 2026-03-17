4ef3dbe05043630908ea9bd39c39cda30ef7c64f
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2026-03-17 11:57:23 +0100

    Create Credential.cs

---
+++ Models/Credential.cs
@@ -0,0 +1,8 @@
+namespace PasswordManager.Models;
+
+public class Credential
+{
+    public string Site { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
